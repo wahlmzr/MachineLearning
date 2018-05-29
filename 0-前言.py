b039bea6e52bd9cc247f26197897fbfd3cb7f758
shuguo <mashuguo@171xue.com>
2018-05-29 11:42:24 +0800

    Revert "Initial commit"
    This reverts commit 4cb501d34988d929749ecc4c246fb0f2c2a3f49b.

--- 0-前言.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 29 11:40:58 2018
-
-@author: Administrator
-"""
-
--- 2-KNN.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 29 11:40:33 2018
-
-@author: Administrator
-"""
-
